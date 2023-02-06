4d587553a43c20defc496d0a20c22ff0c2c680b5
toandq <dauquococ@gmail.com>
2023-02-06 21:19:08 +0700

    ...

--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -1,13 +1,13 @@
 function createSocket(io: any) {
     io.on('connection', (socket: any) => {
         socket.on('join_room', (room: any) => {
-            socket.join('123')
+            socket.join('12sdfsdfsdfsdfsdxxcvxcv3')
             console.log(
                 `User with ID: ${socket.id} joined room: ${typeof room}`
             )
         })
         socket.on('send_message', (data: any) => {
-            socket.broadcast.emit('receive_message', data)
+            socket.to('12sdfsdfsdfsdfsdxxcvxcv3').emit('receive_message', data)
         })
         socket.on('disconnect', () => {
             console.log('User Disconnected', socket.id)
